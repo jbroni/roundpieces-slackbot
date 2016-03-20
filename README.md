ace288b78546a2702e2ed2a3d68fff4823495dac
Jesper Broni Andersen <jba@ccieurope.com>
2016-03-20 14:48:10 +0100

    Check that environment is properly configured before starting bot and provide setup information if it's not

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # roundpieces-slackbot
 A slack bot for administration of roundpieces
+
+## Setup
+Make sure your environment is properly configured. You should set
+
+- `ROUNDPIECES_API_KEY` to the slack bot API token
+- `ROUNDPIECES_ADMIN_USERNAME` to the slack username of your roundpieces administrator
+- `ROUNDPIECES_LIST_PATH` to the absolute path to your list of participants
+
+Afterwards, simply start the bot by running `node bin/startBot.js`
--- bin/startBot.js
+++ bin/startBot.js
@@ -4,10 +4,18 @@ const token = process.env.ROUNDPIECES_API_KEY;
 const adminUserName = process.env.ROUNDPIECES_ADMIN_USERNAME;
 const listPath = process.env.ROUNDPIECES_LIST_PATH;
 
-const roundpiecesBot = new RoundpiecesBot({
-  token: token,
-  adminUserName: adminUserName,
-  listPath: listPath
-});
+if (token && adminUserName && listPath) {
+  const roundpiecesBot = new RoundpiecesBot({
+    token: token,
+    adminUserName: adminUserName,
+    listPath: listPath
+  });
 
-roundpiecesBot.run();
+  roundpiecesBot.run();
+}
+else {
+  console.error(`Environment is not properly configured. Please make sure you have set\n
+      ROUNDPIECES_API_KEY to the slack bot API token
+      ROUNDPIECES_ADMIN_USERNAME to the slack username of your roundpieces administrator
+      ROUNDPIECES_LIST_PATH to the absolute path to your list of participants`);
+}
